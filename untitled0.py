1008652e37da8be00364c77d7eeae652eaac4627
ERKINJON <76995715+Jeysn@users.noreply.github.com>
2021-01-12 19:03:08 +0300

    Add files via upload

---
+++ untitled0.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jan  7 09:54:54 2021
+
+@author: Progress Service
+
+14-dars lug'at bilan ishlash '
+"""
+
+qoshnilar={"d_ism":"daston","d_yosh":"20","d_yil":"2002",
+           "i_ism":"ijodbek","i_yosh":"22","i_yil":"1999",
+           "j_ism":"javlonbek","j_yosh":"25","j_yil":"1996"}
+
+print(f"Ismi {qoshnilar['d_ism'].title()} yoshi {qoshnilar['d_yosh']} da {qoshnilar['d_yil']}-yilda tug'ilgan \
+      Ismi {qoshnilar['i_ism'].title()} yoshi {qoshnilar['i_yosh']} da {qoshnilar['i_yil']}-yilda tug'ilgan \
+      Ismi {qoshnilar['j_ism'].title()} yoshi {qoshnilar['j_yosh']} da {qoshnilar['j_yil']}-yilda tug'ilgan ")
